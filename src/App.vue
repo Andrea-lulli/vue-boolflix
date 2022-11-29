8a83e98ae000f1bb7a05e39aa8033e82495479af
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-11-29 13:05:37 +0100

    inseirito input  e creati data

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <HeaderComp @inputSearch="nomeMethods" />
+    <MainComp />
+  </div>
+</template>
+
+<script>
+import HeaderComp from "./components/HeaderComp.vue";
+import MainComp from "./components/MainComp.vue";
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComp,
+    MainComp,
+  },
+
+  data() {
+    return {
+      SearchUtente: "",
+      ListaFilm: "",
+    };
+  },
+
+  mounted() {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/movie/550?api_key=5e4cb44696283cc119534aa11856e842&query=" + this.SearchUtente)
+
+      .then((resp) => {
+        this.ListaFilm = resp.data;
+      });
+      
+      
+  },
+
+  methods: {
+    nomeMethods(search) {
+      this.SearchUtente = search;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="">
+      <input
+        type="Text"
+        class="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        v-model="search"
+        @keyup="$emit('inputSearch' , search)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+  props: {},
+  data() {
+    return {
+      search: ""
+
+
+
+    };
+  },
+};
+</script>
+
+
+<style scoped lang="scss">
+</style>
